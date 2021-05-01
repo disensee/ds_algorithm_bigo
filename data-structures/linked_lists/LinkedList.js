325e4a6d9e0d121e423d99dff1a5f34ae1eca6bc
disensee <disensee27@gmail.com>
2021-05-01 13:20:10 -0500

    Implementing insert function

--- data-structures/linked_lists/LinkedList.js
+++ data-structures/linked_lists/LinkedList.js
@@ -12,7 +12,12 @@
 //         }
 //     }
 // }
-
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
 class LinkedList {
     constructor(value){
         this.head = {
@@ -25,11 +30,7 @@ class LinkedList {
     }
 
     append(value){
-        const newNode = {
-            value: value,
-            next: null
-        }
-        
+        const newNode = new Node(value);
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
@@ -38,20 +39,63 @@ class LinkedList {
     }
 
     prepend(value){
-        const newNode = {
-            value:  value,
-            next: null
-        }
-
+        const newNode = new Node(value);
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
         
         return this;
     }
+
+    insert(index, value){
+        //Check params
+        if(index === 0){
+            return this.prepend(value);
+        }
+        if(index >= this.length) {
+            return this.append(value);
+        }
+        
+        const newNode = new Node(value);
+
+        const leader = this.traverseToIndex(index-1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this;
+      }
+      
+      traverseToIndex(index) {
+        //Check params
+        if(isNaN(index)){
+            return false;
+        }
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+          currentNode = currentNode.next;
+          counter++;
+        }
+        return currentNode;
+      }
+
+
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(array);
+    }
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList.append(5));
-console.log(myLinkedList.append(16));
-console.log(myLinkedList.prepend(1));
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.printList();
+myLinkedList.insert(3, 66);
+myLinkedList.printList();
