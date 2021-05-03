d1bb5776fedbad526faf0159bfbf6ff766f2d3de
disensee <disensee27@gmail.com>
2021-05-03 16:50:26 -0500

    Starting stacks

---
+++ data-structures/stacksAndQueues/notes.js
@@ -0,0 +1,22 @@
+//Stacks
+
+//Broswer history example
+//google.com -- visited last
+//udemy.com
+//youtube.com --> visited first
+
+//stack representation
+
+//youtube //clicking back would lead to:
+//udemy //clicking back would lead to:
+//google  
+
+
+//Queues
+
+//Waiting list
+//Matt(first) -- Joy -- Samir -- Pavel(last)
+
+//Both stacks and queues can be created with:
+//Arrays
+//Linked Lists
