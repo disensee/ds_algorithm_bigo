5b33fe6244a44cd33514701edc8113dbdd5e262b
disensee <disensee27@gmail.com>
2021-05-09 16:51:22 -0500

    Working on recursion

--- algorithms/recursion/recursion.js
+++ algorithms/recursion/recursion.js
@@ -25,7 +25,7 @@ function findFactorialRecursive(number){
         return number;
     }
     return number * findFactorialRecursive(number - 1);
-}
+} //O(number - 1) -> O(n)
 
 function findFactorialIterative(number){
     let answer = 1;
@@ -33,9 +33,55 @@ function findFactorialIterative(number){
         answer *= i;
     }
     return answer;
-}
+} //O(n)
 
 
 console.log(findFactorialIterative(5));
 console.log(findFactorialRecursive(5));
   
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+function fibonacciIterative(n){ //O(n)
+    let fib = [0, 1];
+    for(i = 2; i <= n; i++){
+        fib.push(fib[i-2] + fib[i - 1]);
+        if(i === n){
+            return fib[i];
+        }
+    }
+
+    return -1;
+}
+  console.log(fibonacciIterative(6));
+  
+  let fib = [0, 1];
+function fibonacciRecursive(n) {// O(2^n) exponential time
+    if(n < 2){
+        return n;
+    }
+    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+}
+  
+console.log(fibonacciRecursive(8));
+
+//Implement a function that reverses a string using iteration...and then recursion!
+function reverseStringIterative(str) {
+    return str.split('').reverse().join('');
+}
+
+
+//Implement a function that reverses a string using iteration...and then recursion!
+let newString = '';
+function reverseStringRecursive(str) {
+    if(str === ""){
+        return newString;
+    }else{
+        return reverseStringRecursive(str.substring(1)) + str.charAt(0);
+    }
+}
+
+console.log(reverseStringIterative('yoyo mastery'));
+//should return: 'yretsam oyoy'
+console.log(reverseStringRecursive('yoyo mastery'));
